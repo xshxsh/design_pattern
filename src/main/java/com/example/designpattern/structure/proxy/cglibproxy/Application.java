67bfec87d4cbd7408f01d48f118c0afa7a21c540
谢仕海 <2477568542@qq.com>
2020-01-14 17:43:42 +0800

    CGLIB动态代理模式

---
+++ src/main/java/com/example/designpattern/structure/proxy/cglibproxy/Application.java
@@ -0,0 +1,17 @@
+package com.example.designpattern.structure.proxy.cglibproxy;
+
+/**
+ * Cglib 动态代理是针对代理的类, 动态生成一个子类, 然后子类覆盖代理类中的方法, 如果是private或是final类修饰的方法,则不会被重写。
+ * CGLIB是一个功能强大，高性能的代码生成包。它为没有实现接口的类提供代理，为JDK的动态代理提供了很好的补充。
+ * 通常可以使用Java的动态代理创建代理，但当要代理的类没有实现接口或者为了更好的性能，CGLIB是一个好的选择。
+ */
+public class Application {
+    public static void main(String[] args) {
+        // 生成 Cglib 代理类
+        Engineer engineerProxy = (Engineer) CglibProxy.getProxy(new Engineer());
+        // 调用相关方法
+        engineerProxy.eat();
+
+        engineerProxy.work();
+    }
+}
---
+++ src/main/java/com/example/designpattern/structure/proxy/cglibproxy/CglibProxy.java
@@ -0,0 +1,40 @@
+package com.example.designpattern.structure.proxy.cglibproxy;
+
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CglibProxy implements MethodInterceptor {
+    private Object target;
+
+    public CglibProxy(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println("###   before invocation");
+        Object result = method.invoke(target, objects);
+        System.out.println("###   end invocation");
+        return result;
+    }
+
+    /**
+     * 生成代理对象
+     * @param target
+     * @return
+     */
+    public static Object getProxy(Object target) {
+        // 通过CGLIB动态代理获取代理对象的过程
+        Enhancer enhancer = new Enhancer();
+        // 设置需要代理的对象
+        enhancer.setSuperclass(target.getClass());
+        // 设置代理人
+        enhancer.setCallback(new CglibProxy(target));
+        // 创建代理对象
+        Object proxy = enhancer.create();
+        return proxy;
+    }
+}
